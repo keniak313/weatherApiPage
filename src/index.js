c6fa60af0a735fdbe65a4c53d75150d6b610d6f2
Pawel <none>
2025-02-07 20:37:32 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,140 @@
-import "./styles.css"
+import "./styles.css";
+import "./searchbar.js";
+import "boxicons";
+
+const virtCrossingApiURL =
+  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+const virtApiKey = "?key=GE65KAXB7DSPKN8W54WJ85E9Q";
+const apiAdditions = "&include=current&unitGroup=metric";
+
+const timer = (ms) => new Promise((res) => setTimeout(res, ms));
+
+export const getWeather = async (country) => {
+  try {
+    const response = await fetch(
+      virtCrossingApiURL + country + virtApiKey + apiAdditions,
+      {
+        mode: "cors",
+      }
+    );
+
+    if (response.ok) {
+      const responseJSON = await response.json();
+      //console.log("OK: " + response.status);
+      return await responseJSON;
+    } else {
+      //console.log("Status: " + response.status);
+      if (response.status === 400) {
+        throw new Error(
+          "Couldnt find anything, make sure you type in correct Location."
+        );
+      } else {
+        throw new Error(response.status);
+      }
+    }
+  } catch (error) {
+    //console.log("error: " + error);
+    return error;
+  }
+};
+
+export function updateInfo(weather) {
+  const sunset = weather.currentConditions.sunset;
+  const sunrise = weather.currentConditions.sunrise;
+  const datetime = weather.currentConditions.datetime;
+
+  const location = document.querySelector(".location");
+  const temp = document.querySelector(".temp");
+  const cond = document.querySelector(".cond");
+
+  const icon = document.querySelector(".weatherIcon .icon");
+  icon.dataset.id = weather.currentConditions.icon;
+  icon.innerHTML = "";
+
+  const dayNight = document.querySelector(".dayNight");
+  dayNight.innerHTML = "";
+
+  location.textContent = `${weather.resolvedAddress}, ${datetime}`;
+  cond.textContent = weather.currentConditions.conditions;
+  temp.textContent = weather.currentConditions.temp;
+  temp.innerHTML += "&degC";
+  document.body.classList = "";
+
+  if (datetime > sunrise && datetime < sunset) {
+    console.log("Day");
+    document.body.classList.add("day");
+    dayNight.innerHTML = "<i class='bx bxs-sun' ></i>";
+    dayNight.classList.add("sun");
+    dayNight.classList.remove("moon");
+  } else {
+    console.log("Night");
+    document.body.classList.add("night");
+    dayNight.innerHTML = "<i class='bx bxs-moon' ></i>";
+    dayNight.classList.add("moon");
+    dayNight.classList.remove("sun");
+  }
+
+  switch (weather.currentConditions.icon) {
+    case "snow":
+      icon.innerHTML = "<i class='bx bx-cloud-snow' ></i>";
+      break;
+    case "rain":
+      icon.innerHTML = "<i class='bx bx-cloud-rain' ></i>";
+      break;
+    case "fog":
+      icon.innerHTML = "<i class='bx bxs-cloud' ></i>";
+      break;
+    case "wind":
+      icon.innerHTML = "<i class='bx bx-wind' ></i>";
+      break;
+    case "cloudy":
+      icon.innerHTML = "<i class='bx bx-cloud' ></i>";
+      break;
+    case "partly-cloudy-day":
+      icon.innerHTML = "<i class='bx bx-cloud' ></i>";
+      break;
+    case "partly-cloudy-night":
+      icon.innerHTML = "<i class='bx bx-cloud' ></i>";
+      break;
+    case "clear-day":
+      icon.innerHTML = "<i class='bx bx-sun' ></i>";
+      break;
+    case "clear-night":
+      icon.innerHTML = "<i class='bx bxs-moon' ></i>";
+      break;
+  }
+}
+
+export function loader(open = true) {
+  const loader = document.querySelector("loader");
+  if (open) {
+    loader.classList.add("open");
+  } else {
+    loader.classList.remove("open");
+  }
+}
+
+async function test() {
+  await timer(200);
+  loader(true);
+  await timer(2000);
+  loader(false);
+}
+
+export function infoAnim(open = true) {
+  const info = document.querySelector("#info");
+  const searchBar = document.querySelector("#searchbar")
+  const error = document.querySelector(".error")
+  if (open) {
+    info.classList.remove("scaleDown");
+    searchBar.classList.remove("scaleDown");
+    error.classList.remove("scaleDown");
+  } else {
+    info.classList.add("scaleDown");
+    searchBar.classList.add("scaleDown");
+    error.classList.add("scaleDown");
+  }
+}
+
+//test();
+//loader(true);
---
+++ src/searchbar.js
@@ -0,0 +1,121 @@
+import "./searchbar.css";
+import { countryList } from "./countriesList";
+import { getWeather, updateInfo, loader, infoAnim } from "./index";
+
+class SearchBar {
+  constructor(searchBar) {
+    this.searchBar = searchBar;
+    this.searchInput = this.searchBar.querySelector("input");
+    this.searchBtn = this.searchBar.querySelector("button");
+    this.list = this.searchBar.querySelector(".list");
+
+    this.createList();
+    this.inputHandler();
+  }
+
+  inputHandler() {
+    this.searchInput.addEventListener("input", (e) => {
+      let children = this.list.childNodes.length;
+      let hidden = 0;
+      this.updateList(true);
+      this.list.childNodes.forEach((c) => {
+        if (!c.dataset.id.includes(this.searchInput.value.toLowerCase())) {
+          c.classList.add("hidden");
+          hidden++;
+        } else {
+          c.classList.remove("hidden");
+        }
+      });
+      if (hidden === children) {
+        this.updateList(false);
+      }
+    });
+
+    this.list.childNodes.forEach((c) => {
+      c.addEventListener("click", (e) => {
+        this.searchInput.value = e.target.textContent;
+        this.updateList(false);
+      });
+      c.addEventListener("focus", (e) => {
+        this.searchInput.value = c.textContent;
+        c.addEventListener("keydown", (e) => {
+          if (e.code === "Enter") {
+            this.updateList(false);
+            this.searchBtn.focus();
+          }
+        });
+      });
+    });
+
+    this.searchInput.addEventListener("focus", (e) => {
+      this.updateList(true);
+    });
+
+    window.addEventListener("click", (e) => {
+      if (!this.searchInput.contains(e.target)) {
+        this.updateList(false);
+      }
+    });
+
+    this.searchBtn.addEventListener("click", async (e) => {
+      e.preventDefault();
+      const error = document.querySelector(".error");
+      error.textContent = "";
+      infoAnim(false);
+      // loader(true);
+      error.classList.add("errorAnim");
+      let weather = await getWeather(this.searchInput.value);
+      infoAnim(true);
+      error.addEventListener("animationend", () => {
+        error.classList.remove("errorAnim");
+      });
+      // loader(false);
+      this.updateList(false);
+      console.log(typeof weather);
+      if (weather.constructor === Object) {
+        this.searchInput.value = "";
+        updateInfo(weather);
+        error.textContent = "";
+      } else {
+        error.textContent = weather;
+      }
+      console.log(weather);
+    });
+  }
+
+  createItem(text) {
+    const item = document.createElement("div");
+    item.classList.add("item");
+    item.dataset.id = text.toLowerCase();
+    item.textContent = text;
+
+    return item;
+  }
+
+  createList() {
+    countryList.forEach((c) => {
+      this.list.appendChild(this.createItem(c));
+    });
+  }
+
+  updateList(open = true) {
+    if (open) {
+      this.list.classList.add("listOpen");
+      this.list.childNodes.forEach((c) => {
+        c.tabIndex = "0";
+      });
+    } else {
+      this.list.classList.remove("listOpen");
+      this.list.childNodes.forEach((c) => {
+        c.tabIndex = "-1";
+      });
+    }
+  }
+}
+
+(function () {
+  const searchBars = document.querySelectorAll("#searchbar");
+  searchBars.forEach((s) => {
+    new SearchBar(s);
+  });
+})();
